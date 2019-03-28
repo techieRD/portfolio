3c20c35928ebc33314a6844cf74d71bc64f73649
Angie Gardi <angiegardi@yahoo.com>
2019-03-27 23:14:29 -0500

    added App.js and index.js to src folder.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div> 
+        <h1>Hello!</h1>
+        <p>My name is Angi. I am a full stack developer.</p>
+        <p>I am always looking forward to working on meaninful projects.</p>
+
+      </div>
+      )
+  }
+}
+
+export default App;
